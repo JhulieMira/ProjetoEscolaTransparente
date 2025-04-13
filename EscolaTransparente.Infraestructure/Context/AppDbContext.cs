a8bed5a08adf25db522ed01d7854be0839afa818
Jhulie Mira <jhuliemira@gmail.com>
2025-04-13 16:55:55 -0300

    Adicionando mapeamento da entidade escola e seus relacionamentos para o entity framework

--- EscolaTransparente.Infraestructure/Context/AppDbContext.cs
+++ EscolaTransparente.Infraestructure/Context/AppDbContext.cs
@@ -19,6 +19,40 @@ namespace EscolaTransparente.Infraestructure.Context
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<EscolaModel>(entity =>
+            {
+                entity.ToTable("Escola");
+                entity.HasKey(e => e.EscolaId);
+                entity.Property(e => e.NivelEnsino).IsRequired();
+                entity.Property(e => e.EscolaId).ValueGeneratedOnAdd();
+                entity.Property(e => e.Nome).IsRequired().HasMaxLength(300);
+                entity.Property(e => e.NotaMedia).IsRequired();
+                entity.Property(e => e.TipoInstituicao).IsRequired();
+                entity.Property(e => e.CNPJ).HasMaxLength(14);
+                entity.Property(e => e.Verificada).HasDefaultValue(false).IsRequired();
+                entity.Property(e => e.DataCadastro).HasDefaultValueSql("GETDATE()");
+
+                entity.HasOne(e => e.Contato)
+                    .WithOne(c => c.Escola)
+                    .HasForeignKey<ContatoModel>(c => c.EscolaId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(e => e.Endereco)
+                    .WithOne(c => c.Escola)
+                    .HasForeignKey<EnderecoModel>(c => c.EscolaId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasMany(e => e.Avaliacoes)
+                      .WithOne(a => a.Escola)
+                      .HasForeignKey(a => a.EscolaId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasMany(e => e.CaracteristicasEscola)
+                      .WithOne(c => c.Escola)
+                      .HasForeignKey(c => c.EscolaId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
             base.OnModelCreating(modelBuilder);
         }
     }
