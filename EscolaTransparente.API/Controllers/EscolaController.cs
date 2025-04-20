439b843f72e64cd7799a028d839e5bafebf550ff
Jhulie Mira <jhuliemira@gmail.com>
2025-04-20 14:24:42 -0300

    Criando escolaController

---
+++ EscolaTransparente.API/Controllers/EscolaController.cs
@@ -0,0 +1,37 @@
+﻿using EscolaTransparente.Application.Data.DataTransferObjects.Escola;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace EscolaTransparente.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EscolaController : ControllerBase  
+    {
+        [HttpGet("ObterEscolaPorId")]
+        public IActionResult ObterEscolaPorId(int escolaId)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("CadastrarEscola")]
+        public IActionResult CadastrarEscola(EscolaDTO escolaDTO)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
