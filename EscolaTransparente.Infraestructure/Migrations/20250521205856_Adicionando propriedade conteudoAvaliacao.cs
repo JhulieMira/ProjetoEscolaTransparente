d97028cd39048bb6b9638b17215ab290cab92886
Jhulie Mira <jhuliemira@gmail.com>
2025-05-21 17:59:39 -0300

    adicionando propriedade conteudo avaliação

---
+++ EscolaTransparente.Infraestructure/Migrations/20250521205856_Adicionando propriedade conteudoAvaliacao.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EscolaTransparente.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandopropriedadeconteudoAvaliacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConteudoAvaliacao",
+                table: "Avaliacao",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConteudoAvaliacao",
+                table: "Avaliacao");
+        }
+    }
+}
